540e440d1fd6419c606efd25d53128e4af466188
AngieLee1 <alee8612@gmail.com>
2018-06-09 15:19:41 -0700

    started RandomMemoryActivity

--- app/src/main/java/hackathon/yesterdaze/RandomMemoryActivity.java
+++ app/src/main/java/hackathon/yesterdaze/RandomMemoryActivity.java
@@ -1,10 +1,36 @@
 package hackathon.yesterdaze;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * Created by angielee on 6/9/18.
  */
 
 public class RandomMemoryActivity extends AppCompatActivity {
+
+    private TextView dateTextView;
+    private TextView memoryTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page);
+
+        dateTextView = (TextView) findViewById(R.id.date_text_view);
+        memoryTextView = (TextView) findViewById(R.id.memory_text_view);
+
+
+        // mem = [from firebase]
+
+
+        dateTextView.setText(mem.getDate());
+        memoryTextView.setText(mem.getText());
+    }
+
 }
